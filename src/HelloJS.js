d195500acb0303d122e0bc0b5e6e3f3a0167147e
Gokhan Kus <gkus1998@hotmail.com>
2025-01-31 12:49:37 +0300

    JS & JSX difference

---
+++ src/HelloJS.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createElement } from 'react'
+
+function Hello() {
+    return (
+        createElement("div", null, "Hello JS")
+    )
+}
+
+export default Hello
