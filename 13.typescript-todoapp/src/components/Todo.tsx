34bab321d9c3af09d0e37edd7a4ac2b227aed9c5
Gokhan Kus <gkus1998@hotmail.com>
2025-02-11 12:10:16 +0300

    typescript-todoapp todo silme

--- 13.typescript-todoapp/src/components/Todo.tsx
+++ 13.typescript-todoapp/src/components/Todo.tsx
@@ -3,18 +3,27 @@ import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { CiCircleCheck } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById } from '../redux/todoSlice';
 
 interface TodoProps {
     todoProps: TodoType
 }
 
 function Todo({ todoProps }: TodoProps) {
+
     const { id, content } = todoProps;
+    const dispatch = useDispatch();
+
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id));
+    }
+
     return (
         <div className='todo'>
             <div>{content}</div>
             <div>
-                <IoIosRemoveCircleOutline className='icons' style={{ marginRight: '5px' }} />
+                <IoIosRemoveCircleOutline onClick={handleRemoveTodo} className='icons' style={{ marginRight: '5px' }} />
                 <CiEdit className='icons' />
             </div>
         </div>
