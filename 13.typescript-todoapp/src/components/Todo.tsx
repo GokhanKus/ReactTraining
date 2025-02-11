0cf3abcc38a21d941c1edf5fdfda5ba3dc675f09
Gokhan Kus <gkus1998@hotmail.com>
2025-02-11 12:43:34 +0300

    typescript-todoapp todo guncelleme

--- 13.typescript-todoapp/src/components/Todo.tsx
+++ 13.typescript-todoapp/src/components/Todo.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { CiCircleCheck } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import { TodoType } from '../types/Types';
 import { useDispatch } from 'react-redux';
-import { removeTodoById } from '../redux/todoSlice';
+import { removeTodoById, updateTodo } from '../redux/todoSlice';
 
 interface TodoProps {
     todoProps: TodoType
@@ -15,16 +15,38 @@ function Todo({ todoProps }: TodoProps) {
     const { id, content } = todoProps;
     const dispatch = useDispatch();
 
+    const [editable, setEditable] = useState<boolean>(false);
+    const [newTodo, setNewTodo] = useState<string>(content);
+
     const handleRemoveTodo = () => {
         dispatch(removeTodoById(id));
     }
 
+    const handleUpdateTodo = () => {
+        const payload: TodoType = {
+            id: id,
+            content: newTodo
+        }
+        dispatch(updateTodo(payload));
+        setEditable(false);
+    }
+
     return (
         <div className='todo'>
-            <div>{content}</div>
+            {
+                editable ?
+                    <input type='text'
+                        style={{ width: '400px', border: 'none', borderBottom: '1px solid lightgrey', outline: 'none' }}
+                        value={newTodo} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} /> :
+                    <div>{content}</div>
+            }
             <div>
                 <IoIosRemoveCircleOutline onClick={handleRemoveTodo} className='icons' style={{ marginRight: '5px' }} />
-                <CiEdit className='icons' />
+                {
+                    editable ?
+                        <CiCircleCheck onClick={handleUpdateTodo} className='icons' /> :
+                        <CiEdit onClick={() => setEditable(true)} className='icons' />
+                }
             </div>
         </div>
     )
