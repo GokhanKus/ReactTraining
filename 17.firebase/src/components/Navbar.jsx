7108f9147ff50759506db780a2de99110d319c1f
Gokhan Kus <gkus1998@hotmail.com>
2025-02-16 12:59:41 +0300

    Firebase kullanici logout

--- 17.firebase/src/components/Navbar.jsx
+++ 17.firebase/src/components/Navbar.jsx
@@ -1,10 +1,25 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../Firebase'
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        await signOut(auth);
+        navigate('/auth');
+    }
+
     return (
         <div className='navbar'>
             <div className='navbar-left'>FIREBASE</div>
-            <div className='navbar-right'>Cikis Yap</div>
+            <div className='navbar-left'>
+                <Link to="/auth" className='navbar-link'>Auth</Link>
+            </div>
+            <div onClick={logout} className='navbar-right'>Cikis Yap</div>
         </div>
     )
 }
